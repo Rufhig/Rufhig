60940d07bbdbbd1e116976500285d88a64de4d78
Alsusu Ahmed <ahmedpyou@outlook.com>
2024-12-02 12:17:47 +0100

    Friet gevonden

--- Friet/js/script.js
+++ Friet/js/script.js
@@ -13,7 +13,7 @@ let count = 0;
 for (let i = 0; i < imgg.length; i++) {
     linker[i].onclick = function () {
         if (i === randomNumber - 1) {
-            imgg[i].src = '../img/gevonden.png'; 
+            imgg[i].src = 'img/gevonden.png'; 
             count++;
             counterTonen.innerHTML = 'gevonden in '+ count +' try';
         } else {
